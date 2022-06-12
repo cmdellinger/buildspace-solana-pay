a943bfedcfba37751b3ebceb2b4e4ef3d2e62cf9
Charles M Dellinger <charles.dellinger@gmail.com>
2022-06-12 15:20:42 -0400

    added products and their download links

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,50 @@
-import React from "react";
-import HeadComponent from '../components/Head';
+import React, { useEffect, useState} from "react";
+import Product from "../components/Products";
+import { PublicKey } from '@solana/web3.js';
+
+import { useWallet } from '@solana/wallet-adapter-react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  
-  
+  // This will fetch the users' public key (wallet address) from any wallet we support
+  const { publicKey } = useWallet();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then(response => response.json())
+        .then(data => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src="https://i.imgur.com/2A0ncp9.jpg" alt="emoji" />
+      
+      <div>
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>
+    </div>
+  );
+
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
+
   return (
     <div className="App">
-      <HeadComponent/>
       <div className="container">
         <header className="header-container">
           <p className="header"> 🌊 Save the Waves 🌊</p>
@@ -18,7 +52,8 @@ const App = () => {
         </header>
 
         <main>
-          <img src="https://i.imgur.com/2A0ncp9.jpg" alt="emoji" />
+                    {/* We only render the connect button if public key doesn't exist */}
+                    {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
         </main>
 
         <div className="footer-container">
